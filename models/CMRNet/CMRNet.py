69ae2fec50e8c089ecc06359cfd003fcf747bb54
Calvin Chen <calvin.chen@asml.com>
2023-07-26 10:40:31 +0800

    update

--- models/CMRNet/CMRNet.py
+++ models/CMRNet/CMRNet.py
@@ -237,8 +237,8 @@ class CMRNet(nn.Module):
         # asd = overlay_imgs(rgb[0], lidar)
         # io.imshow(asd)
         # io.show()
-        print("rgb shape", rgb.shape)
-        print("lidar shape", lidar.shape)
+        # print("rgb shape", rgb.shape)
+        # print("lidar shape", lidar.shape)
 
 
         c11 = self.conv1b(self.conv1aa(self.conv1a(rgb)))
@@ -254,8 +254,13 @@ class CMRNet(nn.Module):
         c16 = self.conv6b(self.conv6a(self.conv6aa(c15)))
         c26 = self.lconv6b(self.lconv6a(self.lconv6aa(c25)))
 
+        print("c16 shape", c16.shape)
+        print("c26 shape", c26.shape)
+
         corr6 = self.corr(c16, c26)      # corr...
         corr6 = self.leakyRELU(corr6)
+
+        print("corr6 shape", corr6.shape)
 
         x = torch.cat((self.conv6_0(corr6), corr6), 1)
         x = torch.cat((self.conv6_1(x), x), 1)
@@ -263,6 +268,7 @@ class CMRNet(nn.Module):
         x = torch.cat((self.conv6_3(x), x), 1)
         x = torch.cat((self.conv6_4(x), x), 1)
 
+        print("before use feat x shape", x.shape)
         if self.use_feat_from > 1:
             flow6 = self.predict_flow6(x)
             up_flow6 = self.deconv6(flow6)
---
+++ print_log.sh
@@ -0,0 +1,11 @@
+
+
+
+
+# input
+rgb shape torch.Size([24, 3, 384, 1280])
+lidar shape torch.Size([24, 1, 384, 1280])
+
+
+
+
