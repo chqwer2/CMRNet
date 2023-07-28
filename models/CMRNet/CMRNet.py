51b743747599a6724bc4ef4527b23985c2cc74e6
Calvin Chen <calvin.chen@asml.com>
2023-07-28 11:34:37 +0800

    prepare pseudo

--- models/CMRNet/CMRNet.py
+++ models/CMRNet/CMRNet.py
@@ -73,38 +73,36 @@ class CMRNet_effn(nn.Module):
         dim = 192
         self.dowmsample_rgb = nn.Sequential(
                 nn.Conv2d(dim, dim, 3, stride=2, padding=1),
-                nn.BatchNorm2d(dim),
+                # nn.BatchNorm2d(dim),
             )
 
         self.dowmsample_lidar = nn.Sequential(
             nn.Conv2d(dim, dim, 3, stride=2, padding=1),
-            nn.BatchNorm2d(dim),
+            # nn.BatchNorm2d(dim),
         )
 
-        # self.ln_rgb = nn.LayerNorm(dim)
-        # self.ln_lidar = nn.LayerNorm(dim)
+        self.ln_rgb = nn.LayerNorm(dim)
+        self.ln_lidar = nn.LayerNorm(dim)
 
 
         self.corr = Correlation(pad_size=md, kernel_size=1, max_displacement=md, stride1=1, stride2=1, corr_multiply=1)
         self.leakyRELU = nn.LeakyReLU(0.1)
-        # output dim = 81
 
 
         # Down sample
-        nd = 465 #(2 * md + 1) ** 2
-        # dd = np.cumsum([128, 128, 96, 64, 32])
-        dd = np.cumsum([465, 128])
+        nd = (2 * md + 1) ** 2
+        dd = np.cumsum([128, 128, 96, 64, 32])
         od = nd
 
 
-        self.conv_after_concat = conv(od, 128, kernel_size=3, stride=1)
-        self.bn_after_concat = nn.BatchNorm2d(128)
+        self.conv6_0 = conv(od, 128, kernel_size=3, stride=1)
 
-        # self.conv6_1 = conv(od + dd[0], 128, kernel_size=3, stride=1)
-        # self.conv6_2 = conv(od + dd[1], 96, kernel_size=3, stride=1)
-        # self.conv6_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
 
-        self.conv6_4 = conv(128, 32, kernel_size=3, stride=1)
+
+        self.conv6_1 = conv(od + dd[0], 128, kernel_size=3, stride=1)
+        self.conv6_2 = conv(od + dd[1], 96, kernel_size=3, stride=1)
+        self.conv6_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
+        self.conv6_4 = conv(od + dd[3], 32, kernel_size=3, stride=1)
 
 
 
@@ -122,15 +120,57 @@ class CMRNet_effn(nn.Module):
             self.conv5_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
             self.conv5_4 = conv(od + dd[3], 32, kernel_size=3, stride=1)
 
+        if use_feat_from > 2:
+            self.predict_flow5 = predict_flow(od + dd[4])
+            self.deconv5 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
+            self.upfeat5 = deconv(od + dd[4], 2, kernel_size=4, stride=2, padding=1)
 
-        ln_dim = 3840
-        self.ln = nn.LayerNorm(3840)
+            od = nd + 96 + 4
+            self.conv4_0 = conv(od, 128, kernel_size=3, stride=1)
+            self.conv4_1 = conv(od + dd[0], 128, kernel_size=3, stride=1)
+            self.conv4_2 = conv(od + dd[1], 96, kernel_size=3, stride=1)
+            self.conv4_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
+            self.conv4_4 = conv(od + dd[3], 32, kernel_size=3, stride=1)
 
-        fc_size = 32 #od + dd[4]
+        if use_feat_from > 3:
+            self.predict_flow4 = predict_flow(od + dd[4])
+            self.deconv4 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
+            self.upfeat4 = deconv(od + dd[4], 2, kernel_size=4, stride=2, padding=1)
+
+            od = nd + 64 + 4
+            self.conv3_0 = conv(od, 128, kernel_size=3, stride=1)
+            self.conv3_1 = conv(od + dd[0], 128, kernel_size=3, stride=1)
+            self.conv3_2 = conv(od + dd[1], 96, kernel_size=3, stride=1)
+            self.conv3_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
+            self.conv3_4 = conv(od + dd[3], 32, kernel_size=3, stride=1)
+
+        if use_feat_from > 4:
+            self.predict_flow3 = predict_flow(od + dd[4])
+            self.deconv3 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
+            self.upfeat3 = deconv(od + dd[4], 2, kernel_size=4, stride=2, padding=1)
+
+            od = nd + 32 + 4
+            self.conv2_0 = conv(od, 128, kernel_size=3, stride=1)
+            self.conv2_1 = conv(od + dd[0], 128, kernel_size=3, stride=1)
+            self.conv2_2 = conv(od + dd[1], 96, kernel_size=3, stride=1)
+            self.conv2_3 = conv(od + dd[2], 64, kernel_size=3, stride=1)
+            self.conv2_4 = conv(od + dd[3], 32, kernel_size=3, stride=1)
+
+        if use_feat_from > 5:
+            self.predict_flow2 = predict_flow(od + dd[4])
+            self.deconv2 = deconv(2, 2, kernel_size=4, stride=2, padding=1)
+
+            self.dc_conv1 = conv(od + dd[4], 128, kernel_size=3, stride=1, padding=1, dilation=1)
+            self.dc_conv2 = conv(128, 128, kernel_size=3, stride=1, padding=2, dilation=2)
+            self.dc_conv3 = conv(128, 128, kernel_size=3, stride=1, padding=4, dilation=4)
+            self.dc_conv4 = conv(128, 96, kernel_size=3, stride=1, padding=8, dilation=8)
+            self.dc_conv5 = conv(96, 64, kernel_size=3, stride=1, padding=16, dilation=16)
+            self.dc_conv6 = conv(64, 32, kernel_size=3, stride=1, padding=1, dilation=1)
+            self.dc_conv7 = predict_flow(32)
+
+        fc_size = od + dd[4]
 
         downsample = 128 // (2**use_feat_from)
-
-
         if image_size[0] % downsample == 0:
             fc_size *= image_size[0] // downsample
         else:
@@ -140,6 +180,7 @@ class CMRNet_effn(nn.Module):
         else:
             fc_size *= (image_size[1] // downsample)+1
 
+        self.ln = nn.LayerNorm(fc_size)
         self.fc1 = nn.Linear(fc_size, 512)
 
         self.fc1_trasl = nn.Linear(512, 256)
@@ -148,7 +189,7 @@ class CMRNet_effn(nn.Module):
         self.fc2_trasl = nn.Linear(256, 3)
         self.fc2_rot = nn.Linear(256, 4)
 
-        self.dropout = nn.Dropout(0.0) #dropout)
+        self.dropout = nn.Dropout(dropout)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
@@ -216,8 +257,8 @@ class CMRNet_effn(nn.Module):
 
 
 
-        c_rgb = self.dowmsample_rgb(rgb_features[4])#(self.ln_rgb(rgb_features[4]))
-        c_lidar = self.dowmsample_lidar(lidar_features[4])#self.ln_lidar(lidar_features[4]))
+        c_rgb = self.dowmsample_rgb(self.ln_rgb(rgb_features[4]))
+        c_lidar = self.dowmsample_lidar(self.ln_lidar(lidar_features[4]))
 
         # print("c16 shape", c16.shape)
         # print("c26 shape", c26.shape)
@@ -234,20 +275,12 @@ class CMRNet_effn(nn.Module):
         # print("corr4 shape", corr4.shape)
 
         x = torch.cat((corr4, c_rgb, c_lidar), 1)   # self.conv6_0(corr4), corr4
-        # print("x cat:", x.shape)
+        print("x cat:", x.shape)
 
-        x = self.conv_after_concat(x)
-        x = self.bn_after_concat(x)
-        x = self.leakyRELU(x) # dim=128
-
-
-
-        # x = torch.cat((self.conv6_1(x), x), 1)
-
-        # x = torch.cat((self.conv6_2(x), x), 1)
-        # x = torch.cat((self.conv6_3(x), x), 1)
-
-        x = self.conv6_4(x)
+        x = torch.cat((self.conv6_1(x), x), 1)
+        x = torch.cat((self.conv6_2(x), x), 1)
+        x = torch.cat((self.conv6_3(x), x), 1)
+        x = torch.cat((self.conv6_4(x), x), 1)
 
         # print("before use feat x shape", x.shape)
         if self.use_feat_from > 1:
@@ -267,11 +300,63 @@ class CMRNet_effn(nn.Module):
             x = torch.cat((self.conv5_3(x), x), 1)
             x = torch.cat((self.conv5_4(x), x), 1)
 
+        if self.use_feat_from > 2:
+            flow5 = self.predict_flow5(x)
+            up_flow5 = self.deconv5(flow5)
+            up_feat5 = self.upfeat5(x)
+
+            warp4 = self.warp(c24, up_flow5*1.25)
+            corr4 = self.corr(c14, warp4)
+            corr4 = self.leakyRELU(corr4)
+            x = torch.cat((corr4, c14, up_flow5, up_feat5), 1)
+            x = torch.cat((self.conv4_0(x), x), 1)
+            x = torch.cat((self.conv4_1(x), x), 1)
+            x = torch.cat((self.conv4_2(x), x), 1)
+            x = torch.cat((self.conv4_3(x), x), 1)
+            x = torch.cat((self.conv4_4(x), x), 1)
+
+        if self.use_feat_from > 3:
+            flow4 = self.predict_flow4(x)
+            up_flow4 = self.deconv4(flow4)
+            up_feat4 = self.upfeat4(x)
+
+            warp3 = self.warp(c23, up_flow4*2.5)
+            corr3 = self.corr(c13, warp3)
+            corr3 = self.leakyRELU(corr3)
+            x = torch.cat((corr3, c13, up_flow4, up_feat4), 1)
+            x = torch.cat((self.conv3_0(x), x),1)
+            x = torch.cat((self.conv3_1(x), x),1)
+            x = torch.cat((self.conv3_2(x), x),1)
+            x = torch.cat((self.conv3_3(x), x),1)
+            x = torch.cat((self.conv3_4(x), x),1)
+
+        if self.use_feat_from > 4:
+            flow3 = self.predict_flow3(x)
+            up_flow3 = self.deconv3(flow3)
+            up_feat3 = self.upfeat3(x)
+
+
+            warp2 = self.warp(c22, up_flow3*5.0)
+            corr2 = self.corr(c12, warp2)
+            corr2 = self.leakyRELU(corr2)
+            x = torch.cat((corr2, c12, up_flow3, up_feat3), 1)
+            x = torch.cat((self.conv2_0(x), x), 1)
+            x = torch.cat((self.conv2_1(x), x), 1)
+            x = torch.cat((self.conv2_2(x), x), 1)
+            x = torch.cat((self.conv2_3(x), x), 1)
+            x = torch.cat((self.conv2_4(x), x), 1)
+
+        if self.use_feat_from > 5:
+            flow2 = self.predict_flow2(x)
+
+            x = self.dc_conv4(self.dc_conv3(self.dc_conv2(self.dc_conv1(x))))
+            #flow2 = flow2 + self.dc_conv7(self.dc_conv6(self.dc_conv5(x)))
+
+
 
         x = x.view(x.shape[0], -1)  # flatten...
+
         x = self.ln(x)
-
-
 
         x = self.dropout(x)
         x = self.leakyRELU(self.fc1(x))
